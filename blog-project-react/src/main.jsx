e2f0064c646232111e40a3a69fff459b418eb4be
MdAkash3135 <mdeaqubmia@gmail.com>
2024-10-24 20:29:30 +0600

    jsx practice session

--- blog-project-react/src/main.jsx
+++ blog-project-react/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+let vr = 'hi'
 createRoot(document.getElementById('root')).render(
+  
   <StrictMode>
     <App />
+    <h1> Todo APP</h1>
+    <p> Chatro League has  banned today {new Date().getFullYear()} </p>
   </StrictMode>,
 )
